8a8b8590a1479850d447a6d452034cd0af182de8
unknown <mauryaprabal7@gmail.com>
2025-01-04 20:07:39 +0530

    removed redundancy from json_write.

--- Task Tracker CLI/dao.py
+++ Task Tracker CLI/dao.py
@@ -10,61 +10,53 @@ def json_read():
     
     return file_data
 
-def json_write_one_task(data):
-    with open(FILE_PATH, 'r') as f:
-        file_data = json.load(f)
-    
-    if str(type(data)) != "<class 'dict'>":
-        file_data.append(data.__dict__)
-    else:
-        file_data.append(data)
+def json_write(task):
+    file_data = json_read()
+
+    file_data.append(task)
 
     with open(FILE_PATH, 'w') as f:
         json.dump(file_data, f, indent=4)
 
-def json_write(data):
-    with open(FILE_PATH, 'w') as f:
-        json.dump(data, f, indent=4)
-
 class TaskDao:
     def add_task(description):
         new_task = Task(description=description)
-        json_write_one_task(new_task)
-        print(new_task.__dict__)
+        json_write(new_task.__dict__)
     
     def update_task(id, description):
         file_data = json_read()
-        json_write([])
+        with open(FILE_PATH, 'w') as f:
+            f.write('[]')
 
         for task in file_data:
             if task['id'] == int(id):
                 task['description'] = description
                 task['updatedAt'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                print(task)
 
-            json_write_one_task(task)
+            json_write(task)
         
     def delete_task(id):
         file_data = json_read()
-        json_write([])
+        with open(FILE_PATH, 'w') as f:
+            f.write('[]')
 
         for task in file_data:
             if task['id'] == int(id):
-                print(task)
                 continue
-            json_write_one_task(task)
+            json_write(task)
             
     
     def change_status(id, status):
         file_data = json_read()
-        json_write([])
+        with open(FILE_PATH, 'w') as f:
+            f.write('[]')
 
         for task in file_data:
             if task['id'] == int(id):
                 task['status'] = status
                 task['updatedAt'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                print(task)
-            json_write_one_task(task)
+
+            json_write(task)
     
     def list_all_tasks():
         print(json.dumps(json_read(), indent=4))
