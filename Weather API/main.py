beb77b14d6e57b56da7cdcbf7bd1f69308f3796c
unknown <mauryaprabal7@gmail.com>
2025-01-13 22:31:29 +0530

    completed Weather API(rate-limiting not implemented). testing is remaining.

--- Weather API/main.py
+++ Weather API/main.py
@@ -1,12 +1,35 @@
+from fastapi import FastAPI
 import requests
 from dotenv import load_dotenv
 import os
+from typing import Optional
+from redis import Redis
+import json
 
 load_dotenv()
-
 WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 
+app = FastAPI()
 
-response = requests.get(f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Pune%2C%20India/2025-01-01/2025-01-09?unitGroup=metric&key={WEATHER_API_KEY}&contentType=json')
+cache = Redis()
 
-print(response.json())
+@app.get('/')
+def index(city: str, country: str, date1: Optional[str]=None, date2: Optional[str]=None):
+    BASE_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+    url = ''
+    if date1 and date2:
+        url = f"{BASE_URL}{city}%2C%20{country}/{date1}/{date2}?unitGroup=metric&key={WEATHER_API_KEY}&contentType=json"
+    elif date1:
+        url = f"{BASE_URL}{city}%2C%20{country}/{date1}?unitGroup=metric&key={WEATHER_API_KEY}&contentType=json"
+    else:
+        url = f"{BASE_URL}{city}%2C%20{country}?unitGroup=metric&key={WEATHER_API_KEY}&contentType=json"
+    
+    if cache.get(url) is not None:
+        response_bytes = cache.get(url)
+        response_string = response_bytes.decode('utf-8')
+        return json.loads(response_string)
+
+    response = requests.get(url)
+    response_string = json.dumps(response.json())
+    cache.set(url, response_string)
+    return response.json()
